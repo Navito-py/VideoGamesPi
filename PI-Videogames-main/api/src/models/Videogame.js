527e0e10e303ed77995a7b9c3e8f438394d19c3a
ivan ariel toneatto <ivan-ariel@hotmail.com>
2022-01-31 23:22:01 -0300

    sequelize terminado

--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    description_raw: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    released: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    platforms: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    background_image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
     },
   });
 };
